a03587a19a288f556a3ed8370d233599805e0534
aknaan <aknaan1510@gmail.com>
2023-11-23 11:27:31 +0200

    added js file with an array of cards

---
+++ scripts/index.js
@@ -0,0 +1,38 @@
+const cardData1 = {
+  name: "grand tetons national park",
+  link: "https://unsplash.com/photos/a-beautiful-view-of-a-mountain-range-with-a-lake-in-the-foreground-llbKxU0xapk",
+};
+
+const cardData2 = {
+  name: "Hector Lake",
+  link: "https://unsplash.com/photos/body-of-water-near-forest-and-mountain-2dIC6JMb_4E",
+};
+
+const cardData3 = {
+  name: "guangxi",
+  link: "https://unsplash.com/photos/a-body-of-water-surrounded-by-mountains-under-a-cloudy-sky-jlIfwzLSDEE",
+};
+
+const cardData4 = {
+  name: "Emerald",
+  link: "https://unsplash.com/photos/a-view-of-a-lake-with-mountains-in-the-background-W-qgMo-YV08",
+};
+
+const cardData5 = {
+  name: "Valley Of The Ten Picks",
+  link: "https://unsplash.com/photos/people-riding-on-boat-on-lake-near-green-trees-and-mountains-during-daytime-nXtCxgYc57c",
+};
+
+const cardData6 = {
+  name: "Hallstatt",
+  link: "https://unsplash.com/photos/houses-near-body-of-water-and-mountain-during-daytime-LKBW3401d-0",
+};
+
+const initialCards = [
+  cardData1,
+  cardData2,
+  cardData3,
+  cardData4,
+  cardData5,
+  cardData6,
+];
